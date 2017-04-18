b1a1f6520a2a618a288c2b45929e403d3a840737
root <root@penta_shcn>
2017-04-18 15:35:38 +0300

    begin pic.php

--- timelapse/pic.php
+++ timelapse/pic.php
@@ -1,7 +1,21 @@
 <?php
 //создаем timelapse
 
-echo "hello";
+//echo "hello";
+
+$path = '/home/pi/beward/penta/2017-04-18/beward_penta/1/';
+
+$list = scandir($path);
+
+
+//var_dump($list);
+
+foreach ($list as $value)
+
+//if ($value == "." || $value=="..")
+
+echo $value . "\n";
+echo "<br>";
 
 
 ?>
