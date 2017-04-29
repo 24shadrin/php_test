f443806847695c50e4c9079a294d109609277e5f
root <root@penta_shcn>
2017-04-29 16:09:30 +0300

    добавлена функция поиска файлов по времени. Нет вывода сообщения что файлы не найдены.

--- timelapse/pic.php
+++ timelapse/pic.php
@@ -142,7 +142,7 @@ $spath = 'penta/';
 $back_url = '<a href="http://192.168.1.200/sm/timelapse/pic.php">back</a>';
 
 
-if ($list = scandir($path, 1))
+if ($list = scandir($path))
 {
 if ($list != false)
 {
@@ -158,33 +158,18 @@ if ($list != false)
 
 	}
 
-//var_dump($files);
-//	sort($files);
-//	var_dump($files);
-/*
-if ($lim == 'all') 
-{
-			foreach($files as $value)
-			{			
-			$picture = $spath . $value;
-			echo "<img src='$picture' width=24% />";
-			}
-echo "<br>";
-echo $back_url;
-			
-}
-else
-{
-	*/
+//	$counter = count($files);
+
+
 $file_count = info_folder();
 				for($i = 0; $i < $lim; $i++)
 				{	
 		if ( $i < $file_count  ) 
 					{			
-			$picture = $spath . $files[$i];
-				echo "<img src='$picture' width=24% />";
+						$picture = $spath . $files[$i + ($file_count - $lim) ];
+						echo "<img src='$picture' width=24% />";
 					}
-//		else echo "<br>";
+
 	
 		
 				}
@@ -234,7 +219,7 @@ ksort($meta);
 echo "<br>";
 echo "имя старшего файла " .$file_name;
 echo "<br>";
-$f_meta = date("F d Y H:i:s", $file_meta);
+$f_meta = date("d m Y H:i:s", $file_meta);
 echo "крайнее движение ".  $f_meta;
 echo  "<br>";
 echo  "<br>";
@@ -242,6 +227,61 @@ $picture = $path . $file_name;
 echo "<img src='$picture' width=24% />";
 echo  "<br>";
 
+}
+
+function show_pic_time($tm)
+{
+$path = '/var/www/sm/timelapse/penta/';
+$spath = 'penta/';
+$back_url = '<a href="http://192.168.1.200/sm/timelapse/pic.php">back</a>';
+
+$files = search_files();
+foreach($files as $value)
+		{
+			$meta[filemtime($path . $value )] = $value;
+			
+		}
+		ksort($meta);
+
+$cur_time = time();
+//var_dump($cur_time);
+foreach($meta as $key => $value)
+		{
+			if ( ($cur_time - $key) < $tm)
+			{
+				$picture = $spath . $value;
+				echo "<img src='$picture' width=24% />";
+			}
+//			else {
+/*				if ( ($cur_time - $key) > $tm)
+				{
+			echo "за выбранный интервал файлов нет";
+		echo "<br>";
+		echo $back_url;
+		return;
+			}
+			*/
+		}
+		
+echo "<br>";
+		echo $back_url;	
+}
+
+function print_form_time()
+{
+echo "показать за ";
+
+echo '<form action="pic.php" method="post">';
+
+echo "<select name='item'>";
+echo "<option value='300'> 5 минут </option>";
+echo "<option value='1800'> 30 минут </option>";
+echo "<option value='3600'> 1 час </option>";
+
+
+echo '<input type="submit" value="применить" />';
+echo "</select>";
+echo "</form>";
 }
 //точка входа в программу--------------------------------------------------------------------------------------
 
@@ -251,8 +291,16 @@ if (isset($_POST['limit']))
 	show_pic($_POST['limit']);
 }
 
+else 
+	
+if (isset($_POST['item']))
+{
+	show_pic_time($_POST['item']);
+}
+
 else{
 print_form();
+print_form_time();
 echo "в папке " . info_folder() . " элементов jpg";
 meta_file();
 
